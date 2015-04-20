1d5a285cb0f185ed7082bfdc519f10975282f613
Nicolas Eeckeloo <neeckeloo@gmail.com>
2015-04-20 14:29:28 +0200

    Clean module class

--- Module.php
+++ Module.php
@@ -1,10 +1,8 @@
 <?php
 namespace Riskio\ScheduleModule;
 
-use Zend\Console\Adapter\AdapterInterface as ConsoleAdapterInterface;
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
-use Zend\Mvc\MvcEvent;
 
 class Module implements AutoloaderProviderInterface, ConfigProviderInterface
 {
@@ -22,38 +20,5 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
-    }
-
-    /**
-     * @param MvcEvent $e
-     */
-    public function onBootstrap(MvcEvent $e)
-    {
-        $application    = $e->getApplication();
-        $serviceManager = $application->getServiceManager();
-
-        /* @var $reportHandler \Riskio\ScheduleModule\Report\ReportHandler */
-        $reportHandler = $serviceManager->get('Riskio\ScheduleModule\Report\ReportHandler');
-
-        $reportThumbnailListener = $serviceManager->get('Riskio\ScheduleModule\Listener\ReportThumbnailListener');
-        $reportStatusListener    = $serviceManager->get('Riskio\ScheduleModule\Listener\ReportStatusListener');
-
-        $reportHandler->getEventManager()->attach($reportThumbnailListener);
-        $reportHandler->getEventManager()->attach($reportStatusListener);
-
-        $reportUpdateListener = $serviceManager->get('Riskio\ScheduleModule\Listener\ReportUpdateListener');
-        $application->getEventManager()->attach($reportUpdateListener);
-    }
-
-    public function getConsoleBanner(ConsoleAdapterInterface $console)
-    {
-        return 'Riskio Report';
-    }
-
-    public function getConsoleUsage(ConsoleAdapterInterface $console)
-    {
-        return [
-            'report update' => 'Update queued report',
-        ];
     }
 }
