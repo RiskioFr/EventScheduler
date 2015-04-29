0017334d28eb5b54e9c42af2a4efc9c92545b65f
Nicolas Eeckeloo <neeckeloo@gmail.com>
2015-04-29 11:13:40 +0200

    Fix unit tests

--- tests/TemporalExpression/FromTest.php
+++ tests/TemporalExpression/FromTest.php
@@ -9,7 +9,7 @@ class FromTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function includes_GivenOlderDate_ShouldReturnTrue()
+    public function includes_GivenMoreRecentDate_ShouldReturnTrue()
     {
         $date = new DateTime('2015-04-12');
         $temporalExpression = new From($date);
@@ -22,7 +22,7 @@ class FromTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function includes_GivenMoreRecentDate_ShouldReturnFalse()
+    public function includes_GivenOlderDate_ShouldReturnFalse()
     {
         $date = new DateTime('2015-04-12');
         $temporalExpression = new From($date);
